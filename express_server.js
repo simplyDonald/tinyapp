95eeb27aa035bad75a4b030bc436d028c476cf41
simplyDonald <donaldabuah@gmail.com>
2021-11-10 21:00:35 +0000

    implemented logic for existing username and empty strings

--- express_server.js
+++ express_server.js
@@ -14,6 +14,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
+const findDbEmail = function(email){
+  for(let userRandomId in users) {
+    if(users[userRandomId].email === email){
+      return users[userRandomId].email;
+    }
+  }
+  return null;
+}
+
+
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -30,19 +54,57 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     urls: urlDatabase
   };
   res.render('urls_index',templateVars);
 });
-
+// const user = findUserByEmail(req.body.email);
+//     if (user) {
+//         res.cookie('id', user.id);
+//         res.redirect(`/`);
+//     } else {
+//         res.send("FAILED LOGIN");
+//     }
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     urls: urlDatabase
   };
   res.render("urls_new",templateVars);
 }); 
+
+app.get("/register", (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+
+  // extract the information that was Submitted with the form
+  const email = req.body.email;
+  const password = req.body.password;
+  const newRandomId = Math.random().toString(36).substring(2,8);
+  if(!email || !password){
+    return res.sendStatus(400);
+  }
+  const found = findDbEmail(email);
+  if(found) {
+    return res.sendStatus(400);
+  }
+  // Add it to the database (jsJokesDb)
+  users[newRandomId] = {
+    id: newRandomId, 
+    email: email, 
+    password: password
+  };
+
+  res.cookie('user_id', newRandomId );
+  // redirect
+  // ask the browser to perform get /jokes
+  res.redirect('/urls');
+
+
+})
 
 app.post('/urls/new', (req, res) => {
 
@@ -89,7 +151,6 @@ app.post("/urls/:shortURL/", (req, res) => {
 
   urlDatabase[shortURL] = longURL;
 
-
  // redirect
   res.redirect('/urls');  
 });
@@ -100,7 +161,9 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { username: req.cookies["username"],shortURL: req.params.shortURL, longURL: urlDatabase.shortURL  };
+  const templateVars = { user: users[req.cookies["user_id"]],
+  shortURL: req.params.shortURL, 
+  longURL: urlDatabase.shortURL  };
   res.render("urls_show", templateVars);
 });
 
